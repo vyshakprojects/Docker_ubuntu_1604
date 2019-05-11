cb755f6788120e9d788fe6729b239ecd8d05f750
vyshakprojects <vyshakm117@gmail.com>
2019-05-11 16:23:03 +0530

    first

---
+++ README.md
@@ -0,0 +1,10 @@
+# Docker_ubuntu_1604
+This script will install Docker in Ubuntu 16.04.
+
+To run the script enter the below command and provide the username.
+
+./script.sh
+
+Please make sure you are logged in as root.
+
+
---
+++ script.sh
@@ -0,0 +1,13 @@
+!#/bin/bash
+clear;
+echo "enter the user name "
+read name
+apt update -y
+apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt update
+apt-cache policy docker-ce
+apt install -y docker-ce
+systemctl status docker
+usermod -aG docker $name
